057a8c161fab3cc759c81832e8a2c131e1f22102
patelhitakshi28 <hpatel2817gmail.com>
2025-03-06 09:46:58 +0100

    added new  code to data.py file in json-sql folder. Two ways to read JSON data. one locally and another is using API

--- json-and-sql/data.py
+++ json-and-sql/data.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 df = pd.read_json('train.json')
 
+dataframe= pd.read_json('https://api.exchangeratesapi.io/v1/latest?access_key=3087a6ccfede888ebca6b1419d2d1f11')
+
 print(df)
+print(dataframe)
